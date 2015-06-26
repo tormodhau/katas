1a10138e7f5b6efcb7ea479b99b16ca7d174105c
Safurudin Mahic <safurum@gmail.com>
2015-06-26 12:48:53 +0200

    Added interface

---
+++ IFizzBuzzKata.cs
@@ -0,0 +1,9 @@
+public interface IFizzBuzzKata
+{
+    /// <summary>
+    /// Give an answer to the current game
+    /// </summary>
+    /// <param name="number">current number in the game sequence</param>
+    /// <returns>appropriate answer to the current number</returns>
+    string Answer(int number);
+}
