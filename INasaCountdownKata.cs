4e1c95b3b10be350ad9c60e436f49f6b67057736
Safurudin Mahic <safurum@gmail.com>
2015-06-26 12:59:24 +0200

    Create INasaCountdownKata.cs

---
+++ INasaCountdownKata.cs
@@ -0,0 +1,9 @@
+public interface INasaCountdownKata
+{
+    /// <summary>
+    /// Issue the NASA countdown
+    /// </summary>
+    /// <param name="start">number to start with</param>
+    /// <returns>array with numbers from 'start' to 0</returns>
+    int[] Countdown(int start);
+}
