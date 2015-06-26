6715be2d5c1bf273af8b74f29a742a6cd197df3a
Safurudin Mahic <safurum@gmail.com>
2015-06-26 12:49:32 +0200

    Delete IFizzBuzzKata.cs

--- IFizzBuzzKata.cs
+++
@@ -1,9 +0,0 @@
-public interface IFizzBuzzKata
-{
-    /// <summary>
-    /// Give an answer to the current game
-    /// </summary>
-    /// <param name="number">current number in the game sequence</param>
-    /// <returns>appropriate answer to the current number</returns>
-    string Answer(int number);
-}
